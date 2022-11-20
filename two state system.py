273c84093efac70cc167c46873766f825a8ab789
Patrik <103080404+WinsomeDeer@users.noreply.github.com>
2022-11-20 22:55:19 +0000

    Update two state system.py

--- two state system.py
+++ two state system.py
@@ -3,9 +3,18 @@ import math
 import scipy
 
 # Vector of beta values.
-B = 1/linspace(0,3,1000)
+B = linspace(0,3,1000)
 
-# Partition function (non interacting and identical particles).
-Z = math.exp(-B)/(1 + math.exp(-B))
+# Average energy function (non interacting and identical particles).
+E = exp(-1/B)/(1 + exp(-1/B))
+CV = diff(E)/diff(B)
 
-
+# Create subplots.
+subplot(2,1,1)
+plot(B,E)
+xlabel('kT'), ylabel('E')
+xmid = 0.5*((B[1:])+B[0:-1])
+subplot(2,1,2)
+plot(xmid, CV)
+xlabel('kT'), ylabel('CV')
+show()
